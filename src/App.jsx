5866c4299023d74a014d3f2eb87272588ddbef82
Vysakhd <vysakh.d@edstem.com>
2023-11-08 00:07:58 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 
+
+
 // import React, { useState } from 'react';
 // import GoalList from './components/GoalList';
 // import './App.css';
@@ -102,9 +104,11 @@
 //   };
 
 //   return (
-//     <div className="container mx-auto p-4">
-//       <h1 className="text-3xl font-bold mb-4 text-center">Goal Management App</h1>
-//       <div className="flex items-center space-x-2">
+//     <div className="container mx-auto p-4 bg-gray-100">
+//       <h1 className="text-3xl font-bold mb-4 text-center text-blue-800">
+//         Goal Management App
+//       </h1>
+//       <div className="flex items-center space-x-2 bg-white p-2 rounded-md mb-4">
 //         <input
 //           type="text"
 //           placeholder="Enter Your Goal"
@@ -140,61 +144,6 @@
 //           Add Goal
 //         </button>
 //       </div>
-//       <div
-//         className={`fixed inset-0 flex items-center justify-center z-50 ${
-//           isModalOpen ? '' : 'hidden'
-//         }`}
-//       >
-//         <div className="modal-container">
-//           <div className="bg-white rounded shadow-lg p-4">
-//             <h2 className="text-lg font-semibold mb-4">
-//               {editGoalId !== null ? 'Edit Goal' : 'Add Goal'}
-//             </h2>
-//             <input
-//               type="text"
-//               placeholder="Enter Your Goal"
-//               className="w-full p-2 mb-2 border rounded"
-//               value={newGoal}
-//               onChange={(e) => setNewGoal(e.target.value)}
-//             />
-//             <input
-//               type="text"
-//               placeholder="Goal Updates"
-//               className="w-full p-2 mb-2 border rounded"
-//               value={newGoalDescription}
-//               onChange={(e) => setNewGoalDescription(e.target.value)}
-//             />
-//             <input
-//               type="date"
-//               placeholder="Start Date"
-//               className="w-full p-2 mb-2 border rounded"
-//               value={newStartDate}
-//               onChange={(e) => setNewStartDate(e.target.value)}
-//             />
-//             <input
-//               type="date"
-//               placeholder="End Date"
-//               className="w-full p-2 mb-2 border rounded"
-//               value={newEndDate}
-//               onChange={(e) => setNewEndDate(e.target.value)}
-//             />
-//             <div className="flex justify-end">
-//               <button
-//                 onClick={saveGoal}
-//                 className="bg-blue-500 text-white p-2 rounded-md mr-2"
-//               >
-//                 Save
-//               </button>
-//               <button
-//                 onClick={closeModal}
-//                 className="bg-red-500 text-white p-2 rounded-md"
-//               >
-//                 Cancel
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
 //       <GoalList goals={goals} onDelete={deleteGoal} onEdit={editGoal} updateProgress={updateProgress} />
 //     </div>
 //   );
@@ -205,7 +154,7 @@
 
 import React, { useState } from 'react';
 import GoalList from './components/GoalList';
-import './App.css';
+// import './App.css';
 
 function App() {
   const [goals, setGoals] = useState([]);
@@ -233,22 +182,11 @@ function App() {
       setNewGoalDescription('');
       setNewStartDate('');
       setNewEndDate('');
-      closeModal();
     }
   };
 
   const deleteGoal = (goalId) => {
     setGoals(goals.filter((goal) => goal.id !== goalId));
-  };
-
-  const editGoal = (goalId) => {
-    setEditGoalId(goalId);
-    const goalToEdit = goals.find((goal) => goal.id === goalId);
-    setNewGoal(goalToEdit.text);
-    setNewGoalDescription(goalToEdit.description);
-    setNewStartDate(goalToEdit.startDate);
-    setNewEndDate(goalToEdit.endDate);
-    openModal();
   };
 
   const openModal = () => {
@@ -258,6 +196,10 @@ function App() {
   const closeModal = () => {
     setModalOpen(false);
     setEditGoalId(null);
+    setNewGoal('');
+    setNewGoalDescription('');
+    setNewStartDate('');
+    setNewEndDate('');
   };
 
   const saveGoal = () => {
@@ -276,6 +218,7 @@ function App() {
               : goal
           )
         );
+        closeModal();
       } else {
         setGoals([
           ...goals,
@@ -288,13 +231,22 @@ function App() {
             endDate: newEndDate,
           },
         ]);
+        setNewGoal('');
+        setNewGoalDescription('');
+        setNewStartDate('');
+        setNewEndDate('');
       }
-      setNewGoal('');
-      setNewGoalDescription('');
-      setNewStartDate('');
-      setNewEndDate('');
-      closeModal();
     }
+  };
+
+  const editGoal = (goalId) => {
+    setEditGoalId(goalId);
+    const goalToEdit = goals.find((goal) => goal.id === goalId);
+    setNewGoal(goalToEdit.text);
+    setNewGoalDescription(goalToEdit.description);
+    setNewStartDate(goalToEdit.startDate);
+    setNewEndDate(goalToEdit.endDate);
+    openModal();
   };
 
   const updateProgress = (goalId, newProgress) => {
@@ -347,6 +299,61 @@ function App() {
         </button>
       </div>
       <GoalList goals={goals} onDelete={deleteGoal} onEdit={editGoal} updateProgress={updateProgress} />
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="modal-container">
+            <div className="bg-white rounded shadow-lg p-4">
+              <h2 className="text-lg font-semibold mb-4">
+                {editGoalId !== null ? 'Edit Goal' : 'Add Goal'}
+              </h2>
+              <input
+                type="text"
+                placeholder="Enter Your Goal"
+                className="w-full p-2 mb-2 border rounded"
+                value={newGoal}
+                onChange={(e) => setNewGoal(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Goal Updates"
+                className="w-full p-2 mb-2 border rounded"
+                value={newGoalDescription}
+                onChange={(e) => setNewGoalDescription(e.target.value)}
+              />
+              <div className="flex space-x-2">
+                <input
+                  type="date"
+                  placeholder="Start Date"
+                  className="w-1/2 p-2 border rounded"
+                  value={newStartDate}
+                  onChange={(e) => setNewStartDate(e.target.value)}
+                />
+                <input
+                  type="date"
+                  placeholder="End Date"
+                  className="w-1/2 p-2 border rounded"
+                  value={newEndDate}
+                  onChange={(e) => setNewEndDate(e.target.value)}
+                />
+              </div>
+              <div className="flex justify-end">
+                <button
+                  onClick={saveGoal}
+                  className="bg-blue-500 text-white p-2 rounded-md mr-2"
+                >
+                  Save
+                </button>
+                <button
+                  onClick={closeModal}
+                  className="bg-red-500 text-white p-2 rounded-md"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
